578b9c6f01b560c09b4ad955b1d6a52e7cbd44b6
Santosh Yadav <Sant7124@gmail.com>
2026-04-21 00:22:01 +0530

    Implement findPreSuc function to find Predecessor and Successor

---
+++ 20-March 2026/Predecessor and Successor.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<Node*> findPreSuc(Node* root, int key) {
+        Node* pre = NULL;
+        Node* suc = NULL;
+        
+        Node* curr = root;
+        
+        while (curr) {
+            if (curr->data < key) {
+                pre = curr;
+                curr = curr->right;
+            } 
+            else if (curr->data > key) {
+                suc = curr;
+                curr = curr->left;
+            } 
+            else {
+                // predecessor
+                if (curr->left) {
+                    Node* temp = curr->left;
+                    while (temp->right)
+                        temp = temp->right;
+                    pre = temp;
+                }
+                
+                // successor
+                if (curr->right) {
+                    Node* temp = curr->right;
+                    while (temp->left)
+                        temp = temp->left;
+                    suc = temp;
+                }
+                break;
+            }
+        }
+        
+        return {pre, suc};
+    }
+};
